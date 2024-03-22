deccbf2421439dba6d1e0fb187f769334b8d74d8
杜麗弥 <rei0409@toreiwakonpyuta.lan>
2024-03-22 17:27:09 +0900

    パスワードをハッシュ化するように実装

--- backend/src/crud/crud.py
+++ backend/src/crud/crud.py
@@ -4,26 +4,37 @@ from sqlalchemy.exc import DBAPIError
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from fastapi import HTTPException
-
+import hashlib
 
 def register_user(session, user_name:str, user_password: str):
-  user_obj = User(
-      name =  user_name,
-      password = user_password
-  )
-  try:
-      session.add(user_obj)
-  except:
-      raise HTTPException(status_code=500, detail='error')
-  session.commit()
-  session.refresh(user_obj)
-  return user_obj
+    # ユーザーの名前がすでに存在しているかを確認
+    user_info = session.query(User).filter(User.name == f"{user_name}").all()
+    if not user_info:
+        register_pass_hash = hashlib.sha256()
+        register_pass_hash.updata(user_password.encode())
+        user_obj = User(
+            name =  user_name,
+            password = register_pass_hash.hexdigest()
+        )
+        try:
+            session.add(user_obj)
+        except:
+            raise HTTPException(status_code=500, detail='error')
+        session.commit()
+        session.refresh(user_obj)
+        return user_obj.name
+
+    else:
+        return "すでにそのユーザー名は登録されています。別のユーザー名を入力してください。"
 
 def get_user(session, user_name:str, user_password: str):
     user_info = session.query(User).filter(User.name == f"{user_name}").all()
     if not user_info:
         return "入力されたユーザー名は見つかりませんでした。"
-    if user_info[0].password == user_password:
+    
+    login_pass_hash = hashlib.sha256()
+    login_pass_hash.updata(user_password.encode())
+    if user_info[0].password == :
         return user_info[0]
     else:
         return "パスワードが間違っています"
