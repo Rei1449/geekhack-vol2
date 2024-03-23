b8dc0b31bfa964eda3ab86ff6cdc4eaa40067275
kshimamura10 <kshimamura1717@gmial.com>
2024-03-23 16:04:41 +0900

    make Ranking page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import Home from "./Home";
 import Test from "./Test";
 
 import Game from "./components/game/Game";
+import Ranking from "./components/game/Ranking";
 
 function App() {
 	return (
@@ -13,6 +14,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/test" element={<Test />} />
 				<Route path="/game" element={<Game />} />
+				<Route path="/ranking" element={<Ranking />} />
 			</Routes>
 		</>
 	);
---
+++ frontend/src/components/game/Ranking.tsx
@@ -0,0 +1,85 @@
+import { ENDPOINT_URL } from "./Constants";
+import { useEffect, useState } from "react";
+import Hai from "./Hai";
+import { HaiInfo } from "../../types/HaiInfo";
+
+type rankingData = {
+    hand: string;
+    id: number;
+    score: number;
+    user_name: string;
+    updated_at: string;
+    created_at: string;
+}
+type rankingDatas = {
+    high: rankingData[];
+    low: rankingData[];
+}
+
+const defaultRankingDatas: rankingDatas = {
+    high: [{
+        hand: "1,2,3",
+        id: 1,
+        score: 10000,
+        user_name: "user",
+        updated_at: "2024",
+        created_at: "2024",
+    }],
+    low: [{
+        hand: "1,2,3",
+        id: 1,
+        score: 10000,
+        user_name: "user",
+        updated_at: "2024",
+        created_at: "2024",
+    }],
+}
+
+const Ranking = () => {
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [rankingDatas, SetRankingData] = useState<rankingDatas>(defaultRankingDatas);
+    const getRanking = async () => {
+        setIsLoading(true);
+        const res = await fetch(
+            ENDPOINT_URL + "ranking",
+            {
+                method: "GET",
+            }
+        );
+        if (res.ok) {
+            const data: rankingDatas = await res.json();
+            SetRankingData(data);
+            console.log(data);
+            setIsLoading(false);
+        } else {
+            console.log("error")
+        }
+    };
+    const a: string[] = ["a", "b", "c"];
+    useEffect(() => {
+        getRanking();
+    }, []);
+    return (
+        <>
+            {isLoading ? (
+                <p>ロード中</p>
+            ) : (
+                <>
+                { rankingDatas.high.map((rankingData) => {
+                    return <div key={rankingData.id}>
+                        <p>{rankingData.user_name}</p>
+                        <p>{rankingData.score}</p>
+                        <div>{ rankingData.hand.split(',').map((hand, index) => {
+                            const haiInfo: HaiInfo = {kind: Number(hand[0]), number: Number(hand[1])}
+                            return <Hai hai={haiInfo} key={index} />
+                        }) }
+                        </div>
+                    </div>
+                    }) }
+                </>
+            )}
+        </>
+    )
+}
+
+export default Ranking
